d4981f89d01a67c3b46a3acc3010474795d99cf3
manish <manishdesetti@gmail.com>
2024-04-04 17:23:44 +0530

    data transformed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from mlProject import logger
 from mlProject.pipeline.data_ingestion_01 import DataIngestionTrainingPipeline
 from mlProject.pipeline.data_validation_02 import DataValidationTrainingPipeline
+from mlProject.pipeline.data_transformation_03 import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -22,3 +23,14 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
